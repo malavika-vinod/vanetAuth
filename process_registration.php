04f0dbeaabdf9df3f67121b483a99246f925e11e
Malavika <malavika-vinod>
2023-12-06 23:47:40 +0530

    added Python linking

--- process_registration.php
+++ process_registration.php
@@ -16,13 +16,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $password = $_POST["password"];
     $lno=$_POST["licenseNo"];
     $vno=$_POST["vehicleNo"];
+
     // Insert data into the 'users' table
     $sql = "INSERT INTO users (name, street_address_1, street_address_2, city, state, postal_code, country,username,password,vehicle_no,license_no)   VALUES ('$name', '$streetAddress', '$streetAddress2', '$city', '$state', '$postalCode', '$country','$user','$password','$vno','$lno')";
     $stmt = $conn->prepare($sql);
     if ($conn->query($sql) === TRUE) {
         // Registration successful
         $user_id = $conn->insert_id; // Get the last inserted user ID
-
+        $pythonScriptPath = 'generate.py';  // Replace with the actual path
+        $command = "python3 $pythonScriptPath $user_id '$user' '$password' '$lno','$vno'";
+        $result = shell_exec($command);
         // Set session variables
         session_start();
         $_SESSION['user_id'] = $user_id;
